d6c9103f7e1f8f2f8d044d34e5b5bffaed5f5a7e
JohanWindahl <johan.windahl@gmail.com>
2018-02-20 20:27:42 +0100

    Update miniproject_solution.R

--- miniproject_solution.R
+++ miniproject_solution.R
@@ -1,52 +1,47 @@
 library(caret)
 
 #Load files
-setwd("E:/Dropbox/SkolaATM/ML-S/ProjectSolution") #@home
-#setwd("C:/Dropbox/SkolaATM/ML-S/ProjectSolution") #@laptop
-
 dataset <- read.csv('training_data.csv',header = T)
 pred <- read.csv('songs_to_classify.csv',header = T)
 levels(dataset$label) <- c(0,1) #Rename levels to: 0=dislike 1=like
 
 
 #Controls
-trainRatio = 0.98 #Data ratio
+trainRatio = 0.8 #Split samples with this ratio: Train/Validation, 0.8 = 80%
 k = 10 #K-fold cross validation
-r = 3  #Cross validation repeats
+r = 3  #K-fold cross validation repeats
 
 createNewModels = TRUE #Calculate new models?
 printResults = TRUE #Print summary?
 plotResults = TRUE #Plot summary?
 checkAllModelsAgainstValidation = TRUE #Evaluate all models?
-seed <- 1 #Set seed 
-set.seed(seed);print(paste0("Seed: ",seed))
+seed <- 10 #Set seed 
+
+set.seed(seed)
+
 
 #Data splitting
 ratioTest = 1 - trainRatio #Test ratio
 trainNORows <- trainRatio*nrow(dataset) #Number of training rows of dataset
 evalNORows <- nrow(dataset) - trainNORows #Number of validation rows of dataset
 
-dataset_shuffled <- dataset[sample(nrow(dataset)),] #Shuffle rows
-dataset_train <- dataset_shuffled[1:trainNORows,] #Training dataset
-dataset_eval <- dataset_shuffled[(trainNORows + 1):nrow(dataset_shuffled),] #Validation dataset
-
-print(paste0("Samples: ",nrow(dataset), 
-             " | Training on rows ", 1,":",  trainNORows, " Ratio:", trainRatio, 
-             " | Validation on rows ", (trainNORows + 1),":",nrow(dataset_shuffled), " Ratio:", ratioTest))
+dataset_shuffled <- dataset[sample(nrow(dataset)),] #Sample rows
+dataset_train <- dataset_shuffled[1:trainNORows,] #Training samples
+dataset_eval <- dataset_shuffled[(trainNORows + 1):nrow(dataset_shuffled),] #Validation samples
 
 
 #Model/Hyper parameters
 metric <- "Accuracy"
 preProcess = c("center", "scale")
-
-
-
 control <- trainControl(method = "repeatedcv", number = k, repeats = r)
-print(paste0("Using K-Fold cross validation, with K=",k,", Repeats=",r))
 model <- label ~ danceability + energy + key + loudness +   mode + speechiness + 
                  acousticness + instrumentalness + liveness + valence + tempo + duration + 
                  time_signature
 
+print(paste0("Seed=",seed, " - Samples: ",nrow(dataset), 
+             " | Training on rows ", 1,":",  trainNORows, " Ratio:", trainRatio, 
+             " | Validation on rows ", (trainNORows + 1),":",nrow(dataset_shuffled), " Ratio:", ratioTest))
+print(paste0("Using K-Fold cross validation, with K=",k,", Repeats=",r))
 
 #Model creation
 if (createNewModels) {
@@ -99,49 +94,40 @@ if (createNewModels) {
   print("GLMNET -> fit.glmnet")
   set.seed(seed)
   fit.glmnet <- train(model, data = dataset_train, method = "glmnet", metric = metric, preProc = c("center", "scale"), trControl = control)
-  
-  # Neural Network
-  print("Neural Network -> fit.nnet")
-  set.seed(seed)
-  #fit.nnet <- train(model, data = dataset_train, method = "nnet", metric = metric, preProc = c("center", "scale"), trControl = control)
 
   
-  # Deep Neural Network
-  print("Deep Neural Network -> fit.dnn")
-  set.seed(seed)
-  #fit.dnn <- train(model, data = dataset_train, method = "dnn", metric = metric, preProc = c("center", "scale"), trControl = control)
-
-
+  allLike <- rep(1,100)
   listOfModels = list(glm = fit.glm, lda = fit.lda, qda = fit.qda, knn = fit.knn, 
                       rf = fit.rf, gbm = fit.gbm, svm = fit.svmRadial, nb = fit.nb, 
-                      glmnet = fit.glmnet)#, nnet = fit.nnet, dnn = fit.dnn)
+                      glmnet = fit.glmnet)
   
   results <- resamples(listOfModels)
 
   print("Models Done")
 }
 
-#Print/Plot results
+#Print results
 if (printResults) {
   results <- resamples(listOfModels)
   print(summary(results))
 }
+
+#Plot results
 if (plotResults) {
   bwplot(results)
-  dotplot(results)
 }
 
 #Validate each model against validation data
 if (checkAllModelsAgainstValidation) {
   for (i in listOfModels) {
     tempModelName <- paste0("fit.",i$method)
-    assign("tempModelPred", predict(eval(parse(text = tempModelName)), dataset_eval[,1:13]))
+    tempModelPred <- predict(eval(parse(text = tempModelName)), dataset_eval[,1:13])
     evalCorrect <- sum(tempModelPred == dataset_eval$label)
     print(paste0(tempModelName, " evaluation: ", evalCorrect, "/",length(tempModelPred), " correct,  ",(evalCorrect/length(tempModelPred)*100),"%"))
   }
 }
 
-
-
 #Production prediction
-resultSongsToClassify <- predict(fit.gbm, pred)
+resultSongsToClassify <- predict(fit.rf, pred)
+print(resultSongsToClassify)
+
